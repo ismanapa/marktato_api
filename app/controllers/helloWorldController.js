47aae8dd7bfa5a0ece13db07043f5aa99f0df3f7
Ismael Navarro <ismanapa@gmail.com>
2018-10-13 23:32:59 +0200

    Inital solution

---
+++ app/controllers/helloWorldController.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+
+// get /api/helloworld/
+router.get('/', (req, res) => {
+  res.send('GET response');
+});
+
+// post /api/helloworld/
+router.post('/', (req, res) => {
+  res.send('POST response');
+});
+
+// put /api/helloworld/
+router.put('/', (req, res) => {
+  res.send('PUT response');
+});
+
+// delete /api/helloworld/
+router.delete('/', (req, res) => {
+  res.send('DELETE response');
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const helloworldController = require('./app/controllers/helloworldController');
+
+app.use('/api/helloworld', helloworldController);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running', process.env.PORT || 3000);
+});

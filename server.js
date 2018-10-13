36dfeba7acb9745b6ef5b5987ba7c05d98ef2fd7
Ismael Navarro <ismanapa@gmail.com>
2018-10-14 01:33:35 +0200

    Added docker support

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const helloworldController = require('./app/controllers/helloworldController');
+const helloworldController = require('./app/controllers/helloWorldController');
 
 app.use('/api/helloworld', helloworldController);
 
